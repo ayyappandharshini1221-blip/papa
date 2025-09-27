e2c9c9ac0544828ce733078e218cfd7935eb3ec3
Ayyappan <ayyappandharshini1221@gmail.com>
2025-09-27 19:00:05 +0000

    have rain in ui/ux

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { PT_Sans } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
+import Rain from '@/components/ui/rain';
 
 const ptSans = PT_Sans({
   subsets: ['latin'],
@@ -28,6 +29,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-headline antialiased', ptSans.variable)}>
+        <Rain />
         {children}
         <Toaster />
       </body>
